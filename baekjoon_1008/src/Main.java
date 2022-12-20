bc8ebeb47313de6c2239af1486ef80f1294ff1b1
GYUEOP <gueeop0219@naver.com>
2022-12-20 13:40:37 +0900

    백준 1008번 성공 및 해설

---
+++ baekjoon_1008/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        // 문제에서 원하는 출력범위를 맞추기 위해서 자료형을 Double형을 사용해야 한다.
+
+
+        System.out.print(a/b);
+
+    }
+}
