d8d17bdb06ec6b24edfa57fc43ce99b7f1573ef3
GYUEOP <gueeop0219@naver.com>
2023-01-14 16:35:33 +0900

    백준 11021번 문제 성공 및 해설

---
+++ baekjoon_11021/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<num+1; i++) {
+
+            st = new StringTokenizer(br.readLine(), " ");
+            int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+
+            sb.append("Case #" + i + ": " + sum + "\n");
+
+        }
+
+        br.close();
+        System.out.println(sb);
+
+    }
+}
