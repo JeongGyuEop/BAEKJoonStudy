9d656af62ee88696599d98d57cf24a6f774bdec4
JeongGyuEop <gueeop0219@naver.com>
2024-08-16 20:28:25 +0900

    baekjoon_1157 problem solved

---
+++ baekjoon_1157/src/Main.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toUpperCase();
+
+        Map<Character, Integer> arr = new HashMap<>();
+        int maxCount = 0;
+        char maxKey = '?';
+
+        for(int i = 0; i < str.length(); i++) {
+            char key = str.charAt(i);
+            if(!arr.containsKey(key)){
+                arr.put(key, 1);
+                if(arr.get(key) > maxCount){
+                    maxCount = arr.get(key);
+                    maxKey = key;
+                } else if(arr.get(key) == maxCount){
+                    maxKey = '?';
+                }
+            } else {
+                arr.put(key, arr.get(key) + 1);
+                if(arr.get(key) > maxCount){
+                    maxCount = arr.get(key);
+                    maxKey = key;
+                } else if(arr.get(key) == maxCount){
+                    maxKey = '?';
+                }
+            }
+        }
+
+        System.out.println(maxKey);
+    }
+}
