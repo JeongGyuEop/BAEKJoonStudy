343579bfa2d4fdcaacee3eb62dbeb04acb04eff2
JeongGyuEop <gueeop0219@naver.com>
2024-08-16 22:26:06 +0900

    beakjoon_1157 update

--- baekjoon_1157/src/Main.java
+++ baekjoon_1157/src/Main.java
@@ -15,26 +15,41 @@ public class Main {
         int maxCount = 0;
         char maxKey = '?';
 
+        // 공부 후 수정한 코드
         for(int i = 0; i < str.length(); i++) {
             char key = str.charAt(i);
-            if(!arr.containsKey(key)){
-                arr.put(key, 1);
-                if(arr.get(key) > maxCount){
-                    maxCount = arr.get(key);
-                    maxKey = key;
-                } else if(arr.get(key) == maxCount){
-                    maxKey = '?';
-                }
-            } else {
-                arr.put(key, arr.get(key) + 1);
-                if(arr.get(key) > maxCount){
-                    maxCount = arr.get(key);
-                    maxKey = key;
-                } else if(arr.get(key) == maxCount){
-                    maxKey = '?';
-                }
+            arr.put(key, arr.getOrDefault(key, 0) + 1);
+
+            int currentCount = arr.get(key);
+            if(currentCount > maxCount){
+                maxCount = currentCount;
+                maxKey = key;
+            } else if(currentCount == maxCount){
+                maxKey = '?';
             }
         }
+
+        // 내가 풀이한 코드
+//        for(int i = 0; i < str.length(); i++) {
+//            char key = str.charAt(i);
+//            if(!arr.containsKey(key)){
+//                arr.put(key, 1);
+//                if(arr.get(key) > maxCount){
+//                    maxCount = arr.get(key);
+//                    maxKey = key;
+//                } else if(arr.get(key) == maxCount){
+//                    maxKey = '?';
+//                }
+//            } else {
+//                arr.put(key, arr.get(key) + 1);
+//                if(arr.get(key) > maxCount){
+//                    maxCount = arr.get(key);
+//                    maxKey = key;
+//                } else if(arr.get(key) == maxCount){
+//                    maxKey = '?';
+//                }
+//            }
+//        }
 
         System.out.println(maxKey);
     }
