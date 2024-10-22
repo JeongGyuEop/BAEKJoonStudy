af94a21e2896ab2856238ec1e2303a7f12d8b7a7
JeongGyuEop <gueeop0219@naver.com>
2024-10-22 19:13:18 +0900

    BaekJoon 10단계-수학은 체육과목입니다.-Solved

--- Level_10/baekjoon_15894/Main.java
+++ Level_10/baekjoon_15894/Main.java
@@ -6,9 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Scanner 객체 생성하여 입력을 받을 준비를 한다.
 		Scanner sc = new Scanner(System.in);
 		
+		// 입력받은 정수 값을 long 형으로 처리하여 둘레를 계산하고 결과 출력
 		System.out.println(sc.nextLong() * 4);
+		
+		// Scanner 자원 해제
+		sc.close();
 
 	}
 
