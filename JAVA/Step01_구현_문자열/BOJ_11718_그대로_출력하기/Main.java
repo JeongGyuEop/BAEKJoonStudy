d47eadac4196660473f193cfc22a002333b38d34
JeongGyuEop <gueeop0219@naver.com>
2025-05-01 13:55:26 +0900

    BOJ 11718 그대로 출력하기 문제 풀이(Java)

---
+++ JAVA/Step01_구현_문자열/BOJ_11718_그대로_출력하기/Main.java
@@ -0,0 +1,20 @@
+package BOJ_11718_그대로_출력하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while((line = br.readLine()) != null ) {
+            System.out.println(line);
+        }
+        
+    }
+    
+}
